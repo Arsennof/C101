665f323b55c32eb9270061307cb9a38c696465f0
Arsennof <97567333+Arsennof@users.noreply.github.com>
2023-07-27 16:43:59 -0700

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,16 @@
+import random
+response = str("y")
+response = str(input("press y to roll a die or press y to exit"))
+while response == "y":
+  no = random.randint(1,6)
+
+if (conditiput.on):
+
+
+
+
+
+
+
+
+
